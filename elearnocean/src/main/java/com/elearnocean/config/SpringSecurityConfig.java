5c6cd5d4bbdee156abd49cfbd7e57eb12f4d994c
Surendra <b.surendra310@gmail.com>
2018-07-16 23:49:53 +0530

    Thymeleaf page navigations

--- elearnocean/src/main/java/com/elearnocean/config/SpringSecurityConfig.java
+++ elearnocean/src/main/java/com/elearnocean/config/SpringSecurityConfig.java
@@ -23,10 +23,12 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		http.csrf().disable().authorizeRequests().antMatchers("/", "/login", "/index").permitAll()
-				.antMatchers("/admin/**").hasAnyRole("ADMIN").antMatchers("/user/**").hasAnyRole("USER").anyRequest()
-				.authenticated().and().formLogin().loginPage("/login").permitAll().and().logout().permitAll().and()
-				.exceptionHandling().accessDeniedHandler(accessDeniedHandler);
+		http.csrf().disable().authorizeRequests()
+				.antMatchers("/", "/login", "/index", "/question", "/generateQuestion", "/importQuestion",
+						"/downloadQuestion")
+				.permitAll().antMatchers("/admin/**").hasAnyRole("ADMIN").antMatchers("/user/**").hasAnyRole("USER")
+				.anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
+				.permitAll().and().exceptionHandling().accessDeniedHandler(accessDeniedHandler);
 	}
 
 	@Autowired
@@ -39,7 +41,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	// Spring Boot configured this already.
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+		web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**", "/dist/**",
+				"/plugins/**");
 	}
 
 }
--- elearnocean/src/main/java/com/elearnocean/controller/DefaultController.java
+++ elearnocean/src/main/java/com/elearnocean/controller/DefaultController.java
@@ -6,24 +6,44 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DefaultController {
 
-    @GetMapping("/")
-    public String login1() {
-        return "/login";
-    }
+	@GetMapping("/")
+	public String login1() {
+		return "/login";
+	}
 
-    @GetMapping("/login")
-    public String login() {
-        return "/login";
-    }
-    
-    @GetMapping("/index")
-    public String index() {
-        return "/index";
-    }
+	@GetMapping("/login")
+	public String login() {
+		return "/login";
+	}
 
-    @GetMapping("/error")
-    public String error403() {
-        return "/error/403";
-    }
+	@GetMapping("/index")
+	public String index() {
+		return "/index";
+	}
+
+	@GetMapping("/question")
+	public String question() {
+		return "/question";
+	}
+
+	@GetMapping("/generateQuestion")
+	public String generateQuestion() {
+		return "/generateQuestion";
+	}
+
+	@GetMapping("/importQuestion")
+	public String importQuestion() {
+		return "/importQuestion";
+	}
+
+	@GetMapping("/downloadQuestion")
+	public String downloadQuestion() {
+		return "/downloadQuestion";
+	}
+
+	@GetMapping("/error")
+	public String error403() {
+		return "/error/403";
+	}
 
 }
